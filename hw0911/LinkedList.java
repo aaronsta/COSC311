66efc130f4a62e4201303d119507f3ec30a3383d
Aaron Stark <aaronstark@Aarons-MacBook-Air.local>
2018-09-18 09:59:27 -0400

    added toChange

--- hw0911/LinkedList.java
+++ hw0911/LinkedList.java
@@ -1,14 +1,14 @@
 /*
  * Aaron Stark
- * https://github.com/aaronsta/COSC311/tree/master/hw0911
- * COSC 311
  * hw0911
- * FALL 2018
+ * https://github.com/aaronsta/COSC311/tree/master/hw0911
+ * COSC 311, FALL 2018
  * 
+ * Citations:
  * deleteGreaterThanOrEqToAverage and deleteLessThanOrEqToAverage are based on 	https://stackoverflow.com/questions/36587186/delete-multiple-nodes-from-linked-list-java
 	under "specifically for this case you want to remove a node." in 	answer from Apr 13 '16 at 2:33
 * insertLast is  based on code from the GTG textbook, p. 127 
-*  displayList is based on code from the Lafore, Data Structures and Algorithms in Java textbook, p. 204
+*  toString() is based on Prof. Haynes code here https://emunix.emich.edu/~haynes/CommonCourseMaterial/311/LinkedListDelete/LinkedList.java
  */
 
 public class LinkedList {
@@ -102,32 +102,30 @@ public class LinkedList {
 		return this;
 	}
 
-	public void displayList(){
-		Link current = head;
-		while (current != null){ 
-			System.out.print(current.data + " ");
-			current = current.next;
-		}
-		System.out.println("");
-	}
-	
+	public String toString() {
+        String str = "";
+        for (Link p = head; p != null; p = p.next) {
+        	 str += p.data + " ";
+        }
+        return str;
+    }
 
 	public static void main(String[] args) {
 		LinkedList aList = new LinkedList();
-		double [] arrDbl = {100.0,  10.0,  15.0,  20.0,  200.0,  30.0,  40.0,  300.0};
+		double [] arrDbl = { 100.0,  10.0,  15.0,  20.0,  200.0,  30.0,  40.0,  300.0 };
 		for(int i = 0; i < arrDbl.length; i++){
 			aList.insertLast(arrDbl[i]);
 		}
 		System.out.println("Starting List:");
-		aList.displayList();
+		System.out.println(aList.toString());
 		
 		System.out.print("Average: ");
 		aList.computeAverage();
 		System.out.println(aList.getAverage());
 		
 		aList.deleteLessThanOrEqToAverage();
-		System.out.println("Ending List:");
-		aList.displayList();
+		System.out.println("Ending List (less than or equal to average):");
+		System.out.println(aList.toString());
 		
 	}
 
